cd565034488ebfccbbb697f0d44fd66f1e19f989
Илья Манютин <chem-ixtrems@mail.ru>
2022-10-24 11:09:04 +0300

    В БД создал метод и тип обьекта

---
+++ LabMethods/Models/DBModels/TypeObject.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LabMethods.Models.DBModels
+{
+    internal class TypeObject
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; } = null!;
+
+        //public virtual ICollection<LabMethod> TypeObjects { get; set; } = Array.Empty<LabMethod>();
+    }
+}
